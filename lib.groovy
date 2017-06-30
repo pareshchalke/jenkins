bfab3daa7250704d5a46032a1d4dddb3f97403bc
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-06-30 14:19:54 +0100

    commit

--- lib.groovy
+++ lib.groovy
@@ -1,27 +1,29 @@
 @NonCPS
 
-def getpostdata = { String str ->
-  remoteUri = str
-  repodomain = str.split('/')
-  reponame = repodomain[2].replace(".","-")
-  def writer = new StringWriter()
-  def builder = new groovy.json.JsonBuilder()
-  def root = builder.data {
-    repoType "proxy"
-    id "$reponame"
-    name "$reponame"
-    providerRole "org.sonatype.nexus.proxy.repository.Repository"
-    exposed true
-    provider "maven2"
-    format "maven2"
-    repoPolicy "RELEASE"
-    checksumPolicy "WARN"
-    remoteStorage {
-      remoteStorageUrl "$remoteUri"
-      authentication null
-      connectionSettings null
+def getpostdata(String url) {
+  def getpostdata = { url ->
+    remoteUri = url
+    repodomain = url.split('/')
+    reponame = repodomain[2].replace(".","-")
+    def writer = new StringWriter()
+    def builder = new groovy.json.JsonBuilder()
+    def root = builder.data {
+      repoType "proxy"
+      id "$reponame"
+      name "$reponame"
+      providerRole "org.sonatype.nexus.proxy.repository.Repository"
+      exposed true
+      provider "maven2"
+      format "maven2"
+      repoPolicy "RELEASE"
+      checksumPolicy "WARN"
+      remoteStorage {
+        remoteStorageUrl "$remoteUri"
+        authentication null
+        connectionSettings null
+      }
     }
+    def json = JsonOutput.toJson(root)
+    return json
   }
-  def json = JsonOutput.toJson(root)
-  return json
 }
