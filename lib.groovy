a82eab31289bad74faf057c5063689773b9b5304
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-06-30 14:27:27 +0100

    commit

--- lib.groovy
+++ lib.groovy
@@ -1,27 +1,5 @@
 @NonCPS
 
 def getpostdata (String url) {
-  remoteUri = url
-  repodomain = url.split('/')
-  reponame = repodomain[2].replace(".","-")
-  def writer = new StringWriter()
-  def builder = new groovy.json.JsonBuilder()
-  def root = builder.data {
-    repoType "proxy"
-    id "$reponame"
-    name "$reponame"
-    providerRole "org.sonatype.nexus.proxy.repository.Repository"
-    exposed true
-    provider "maven2"
-    format "maven2"
-    repoPolicy "RELEASE"
-    checksumPolicy "WARN"
-    remoteStorage {
-      remoteStorageUrl "$remoteUri"
-      authentication null
-      connectionSettings null
-    }
-  }
-  def json = JsonOutput.toJson(root)
-  return json
+  println url
 }
