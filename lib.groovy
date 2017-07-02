ce36a878df323d2fe3d5b7054d93a768187bfc9a
Paresh Chalke <pareshchalke@Administrators-MacBook-Pro.local>
2017-07-02 23:32:00 +0100

    commit

--- lib.groovy
+++ lib.groovy
@@ -1,3 +1,5 @@
+import groovy.json.*
+
 def test1() {
   println "test1"
 }
@@ -11,24 +13,76 @@ def getnexusrepo(String url) {
         repo = it.remoteUri
         repo && nexuslist << it.remoteUri
     }
+    //println nexuslist
     return nexuslist
 }
 
-def generatelist(String nexuslist, String repofile) {
+def generatelist(String[] nexusrepo, String repofile) {
     def newlist = []
     def workspace = env.WORKSPACE
     def repolist = new File("${workspace}/${repofile}")
     def lines = repolist.readLines()
     lines.each { String line ->
-        value = nexuslist.contains(line)
-        if (!value) {
+        value = nexusrepo.contains(line)
+        println value
+        if (value == false) {
             newlist << line
         }
     }
+    //println newlist
     return newlist
 }
 
-def test2() {
-  println "test2"
+@NonCPS
+def generatepostdata(String str) {
+    remoteUri = str
+    repodomain = str.split('/')
+    reponame = repodomain[2].replace(".","-")
+    //def writer = new StringWriter()
+    //def builder = new groovy.json.JsonBuilder()
+    def body = [
+        displayName: "smoketest",
+        description: "forsmoketesting",
+        genusTypeId: "type"]
+    def builder = new JsonBuilder()
+    def root = builder.data {
+        repoType("proxy")
+        id("$reponame")
+        name("$reponame")
+        providerRole("org.sonatype.nexus.proxy.repository.Repository")
+        exposed(true)
+        provider("maven2")
+        format("maven2")
+        repoPolicy("RELEASE")
+        checksumPolicy("WARN")
+        remoteStorage(
+            remoteStorageUrl: "$remoteUri",
+            authentication: null,
+            connectionSettings: null
+        )
+    }
+    //def json = new groovy.json.JsonBuilder()
+    //def root = json.build { build_number "world"}
+    return builder.toString()
+    //def json = JsonOutput.toJson(root)
+    //return json
 }
+
+def createrepo(String[] clist) {
+    //println createlist
+    clist.each {
+        println it
+    }
+    //def postdata = generatepostdata("http://heelo.com/aseas/")
+    //println postdata
+    for (i = 0; i <clist.size(); i++) {
+        println clist[i]
+    }
+    clist.each { line ->
+        //println line
+        //def postdata = generatepostdata("${item}")
+        //println postdata
+    }
+}
+
 return this
